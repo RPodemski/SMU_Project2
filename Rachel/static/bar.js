c6b816ab4a7499cb3f40741c0fa46a899195a677
RPodemski <70305161+RPodemski@users.noreply.github.com>
2021-02-13 11:41:22 -0600

    update

--- Rachel/static/bar.js
+++ Rachel/static/bar.js
@@ -42,26 +42,27 @@ function buildBarZip(data) {
         averages.push({ "zipcode": zip, "avg_score": avg });
     });
 
+
     averages = averages.sort(function(a, b) {
         return a.avg_score - b.avg_score;
     });
 
-    var northED = ["75230", "75216", "75224"];
-    var southED = ["75208"];
-    var northWD = ["75212"];
-    var southWD = ["75241"];
+    var northWD = ["75075", "75093", "75080", "75252", "75287", "75007", "75248", "75001", "75006", "75019", "75240", "75254", "75244", "75234", "75063", "75251", "75230", "75229", "75225", "75220", "75205", "75209", "75235", "75219", "75021", "75280", "75044", "75024", "75221", "75081", "75244", "78254", "76244", "75057", "75056", "75281", "78216", "76092", "75289", "75038", "75082", "75250", "752203832", "75220-1314", "752302426", "752096384", "75201-2206", "75230-6114", "75720", "7229", "7520-1", "75013", "72535"];
+    var southWD = ["75247", "75207", "75212", "75208", "75211", "75050", "75051", "75224", "75233", "75236", "75052", "75232", "75237", "75116", "75249", "75115", "75104", "75037", "75061", "78232", "75213", "75181", "752124029", "75212-4112", "15751"];
+    var northED = ["75243", "75231", "75238", "75206", "75214", "75218", "75228", "75150", "75182", "75043", "75088", "75089", "75098", "75173", "75087", "75032", "75223", "75204", "75028", "75256", "75041", "75049", "75039", "77006", "75243-4217", "752435219", "75228-3007", "75228-6371", "752062802"];
+    var southED = ["75203", "75216", "75241", "75215", "75210", "75227", "75217", "75180", "75253", "75159", "75126", "75201", "75202", "75226", "75246", "75261", "75270", "75030", "75239", "75149", "75034", "75042", "75146", "75217-2360", "75216-6751", "75236-1078", "75127", "752172042", "73253", "75134"];
 
 
     var colors = [];
     averages.forEach(function(i) {
         if (northED.includes(i.zipcode) == true) {
-            colors.push("green");
+            colors.push("#00916E");
         } else if (southED.includes(i.zipcode) == true) {
-            colors.push("blue");
+            colors.push("#FFCF00");
         } else if (northWD.includes(i.zipcode) == true) {
-            colors.push("purple");
+            colors.push("#EE6123");
         } else if (southWD.includes(i.zipcode) == true) {
-            colors.push("pink");
+            colors.push("#FA003F");
         } else {
             colors.push("black");
         }
@@ -111,11 +112,11 @@ function buildBarReason(data) {
     var colors2 = [];
     averages2.forEach(function(i) {
         if (i.reason == "Routine") {
-            colors2.push("green");
+            colors2.push("#00916E");
         } else if (i.reason == "Follow-up") {
-            colors2.push("blue");
+            colors2.push("#FFCF00");
         } else if (i.reason == "Complaint") {
-            colors2.push("purple");
+            colors2.push("#FA003F");
         } else {
             colors2.push("black");
         }
@@ -126,7 +127,7 @@ function buildBarReason(data) {
         y: averages2.map(x => x.avg_score),
         type: 'bar',
         marker: {
-            color: colors2,
+            color: colors2
         }
     }];
 
